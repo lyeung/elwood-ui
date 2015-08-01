bf600bbf3b9cde1d47b456a87d37494403688b63
Lenming Yeung <lenming.yeung@gmail.com>
2015-08-02 01:22:16 +1000

    Initial commit.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name elwoodUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the elwoodUiApp
+ */
+angular.module('elwoodUiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
