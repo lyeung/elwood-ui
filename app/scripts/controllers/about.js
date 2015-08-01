c26b3504acb5153aefe36b8d2e61cfe9b3730a45
Lenming Yeung <lyeung@abox.local>
2015-08-02 01:22:16 +1000

    Initial commit.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name elwoodUiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the elwoodUiApp
+ */
+angular.module('elwoodUiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
